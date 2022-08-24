7d9231fb452801d2ed88cde9044b44e5e91d7ab1
liqi <lidaqi962464@qq.com>
2022-08-24 13:51:00 +0800

    添加助手函数

--- lib/utils/util.go
+++ lib/utils/util.go
@@ -2,10 +2,12 @@ package utils
 
 import (
 	"github.com/pkg/errors"
+	"math/rand"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
 // GetProgramName 获取程序名称
@@ -33,4 +35,33 @@ func GetProgramName() (string, error) {
 	}
 
 	return string(strProgram[:]), nil
+}
+
+// RandomMixString 随机生成字符串(英文数字混合)
+func RandomMixString(l int) string {
+	str := "0123456789AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
+	bytes := []byte(str)
+	var result []byte = make([]byte, 0, l)
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < l; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return BytesToString(result)
+}
+
+// RandomNumberString 随机生成字符串
+func RandomNumberString(l int) (result string) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	var tmp int
+	i := 0
+	for i < l {
+		tmp = r.Intn(10)
+		if i == 0 && tmp == 0 {
+			// 首字符为0，跳过
+			continue
+		}
+		result += Int2String(tmp)
+		i++
+	}
+	return
 }
